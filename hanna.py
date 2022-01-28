6c58eea1930e663058bf9498e6cb4750b0bd8cd3
Hanna Benarroch <hanna.benarroch@mines-paristech.fr>
2022-01-28 14:01:47 +0100

    Update hanna.py

--- hanna.py
+++ hanna.py
@@ -2,28 +2,26 @@ import random
 from random import randint
 import numpy as np
 
-nb_lines = 30
-nb_columns = 30
+def create_salle(nb_lines, nb_columns):
+    salle = []
+    for i in range(nb_lines):
+        salle += [[' ' for k in range(nb_columns)]]
 
-screen = []
-for i in range(nb_lines):
-    screen += [[' ' for k in range(nb_columns)]]
+    nb_salles = 15
 
-x,y = random.randint(1, nb_columns-2), random.randint(1, nb_lines-2)
-vertical = random.randint(2, nb_lines-y-1)
-horizontal = random.randint(2, nb_columns-x-1)
-
-for k in range(horizontal+1) :
-    screen[x][y+k] = '-'
-    screen[x+vertical][y+k] = '-'
-for j in range(1, vertical) :
-    screen[x+j][y] = '|'
-    screen [x+j][y+horizontal] = '|'
-for k in range(1, vertical):
-    for j in range(1, horizontal):
-        screen[x+k][y+j] = '.'
-
-for line in screen :
-    print(line)
-
-
+    for numero in range(nb_salles):
+        x,y = random.randint(2, nb_columns-2), random.randint(2, nb_lines-2)
+        if salle[x][y] == ' ' and salle[x-1][y] == ' ' and salle[x][y-1] == ' ' and salle[x-1][y-1] == ' ' and 2<nb_lines-x-2 and 2<nb_columns-y-2:
+            vertical = random.randint(2, nb_lines-x-2)
+            horizontal = random.randint(2, nb_columns-y-2)
+            if all([salle[x+l][y+i]==' ' for i in range(horizontal+2) for l in range(vertical+2)]):
+                for k in range(horizontal+1) :
+                    salle[x][y+k] = '-'
+                    salle[x+vertical][y+k] = '-'
+                for j in range(1, vertical) :
+                    salle[x+j][y] = '|'
+                    salle [x+j][y+horizontal] = '|'
+                for k in range(1, vertical):
+                    for j in range(1, horizontal):
+                        salle[x+k][y+j] = '.'
+    return salle
